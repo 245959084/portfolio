0d21bfc0a9f92f635ec52cb95e0088bb4f65ca14
JingJun <billy.ni222@gmail.com>
2025-01-24 03:17:56 -0300

    nav

--- global.js
+++ global.js
@@ -4,6 +4,7 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+/** 
 navLinks = $$("nav a")
 
 let currentLink = navLinks.find(
@@ -12,4 +13,20 @@ let currentLink = navLinks.find(
 
 if (currentLink) {
     currentLink.classList.add('current');
+} **/
+
+let pages = [
+    {url: "https://245959084.github.io/portfolio/", title: 'Home'},
+    {url: "https://245959084.github.io/portfolio/contact/index.html", title: 'Contact'},
+    {url: "https://245959084.github.io/portfolio/projects/index.html", title: "Projects"},
+    {url: "https://245959084.github.io/portfolio/CV/index.html", title: "Resume"}
+]
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages){
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
