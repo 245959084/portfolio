4c24dc04ce038d7ecfad9c5fe5d237b4624eb4c8
JingJun <billy.ni222@gmail.com>
2025-01-31 08:03:20 -0300

    finha

--- global.js
+++ global.js
@@ -76,4 +76,52 @@ select.addEventListener('input', function (event) {
     console.log(localStorage.colorScheme);
 });
 
+//lab4 step1.2
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);  
+        if (!response.ok){
+            throw new Error(`Failed to fetch project: ${response.statusText}`)
+        }
+        const data = await response.json();
+        return data;
+    } 
+    catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+// const test  = await fetchJSON('../lib/projects.json');
+// console.log(test);
+
+//lab4 step1.3
+export function renderProjects(project, containerElement, headingLevel = 'h2'){
+    if (!(containerElement instanceof HTMLElement)){ //runs of container is invalid
+        console.error('Invalid container element provided.');
+        return; 
+    }
+    if (!Array.isArray(project)){
+        console.error("Invalid projects data.");
+        return;
+    }
+    containerElement.innerHTML = ''; //precents duplicate elements
+    for (let proj of project){//run for each project
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <${headingLevel}>${proj.title}</${headingLevel}>
+            <img src="${proj.image}" alt="${proj.title}">
+            <p>${proj.description}</p>
+            `;
+        containerElement.appendChild(article);
+    }
+}
+// const contain = document.querySelector(".projects")
+// const test2 = renderProjects(test, contain, 'h1');
+// console.log(test2);
+
+//lab4 step3.2
+export async function fetchGitHubData(username){
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
 
---
+++ projects/projects.js
@@ -0,0 +1,16 @@
+//1.4 lab4
+import { fetchJSON, renderProjects } from '../global.js';
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+const result = renderProjects(projects, projectsContainer, 'h2');
+
+
+// const articles = document.querySelectorAll('article');
+
+// articles.forEach(article => {
+//     article.classList.add('project-article'); // Adds the class "project-article" to each article
+// });
+
+const a = document.querySelectorAll('article');
+const header = document.querySelector('h1');
+header.textContent =`${a.length} Projects`;
