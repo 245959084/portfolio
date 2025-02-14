cac66c4a2c4701eb2a05b430b00953a0a3fab91b
JingJun <billy.ni222@gmail.com>
2025-02-14 18:15:33 -0300

    hahahaha

--- Meta/main.js
+++ Meta/main.js
@@ -1,49 +1,60 @@
-//lab6 step 1
+// //lab6 step 1
+// let data = [];
+// let commits = [];
+
+// async function loadData() {
+//     data = await d3.csv('loc.csv', (row) => ({
+//       ...row,
+//       line: Number(row.line), // or just +row.line
+//       depth: Number(row.depth),
+//       length: Number(row.length),
+//       date: new Date(row.date + 'T00:00' + row.timezone),
+//       datetime: new Date(row.datetime),
+//     }));
+//     processCommits();
+//     console.log(commits)
+//   }   //This function convert all string data into correspond type of data
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//     await loadData();
+//     commits = d3.groups(data, (d) => d.commit);
+// });
+
+// function processCommits() {
+//     commits = d3
+//       .groups(data, (d) => d.commit)
+//       .map(([commit, lines]) => {
+//         let first = lines[0];
+//         let { author, date, time, timezone, datetime } = first;
+//         let ret = {
+//           id: commit,
+//           url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+//           author,
+//           date,
+//           time,
+//           timezone,
+//           datetime,
+//           hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+//           totalLines: lines.length,
+//         };
+  
+//         Object.defineProperty(ret, 'lines', {
+//           value: lines,
+//           // What other options do we need to set?
+//           // Hint: look up configurable, writable, and enumerable
+//         });
+  
+//         return ret;
+//       });
+//   }
+
 let data = [];
-let commits = [];
 
 async function loadData() {
-    data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line), // or just +row.line
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-    processCommits();
-    console.log(commits)
-  }   //This function convert all string data into correspond type of data
+  data = await d3.csv('loc.csv');
+  console.log(data);
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await loadData();
-    commits = d3.groups(data, (d) => d.commit);
+  await loadData();
 });
-
-function processCommits() {
-    commits = d3
-      .groups(data, (d) => d.commit)
-      .map(([commit, lines]) => {
-        let first = lines[0];
-        let { author, date, time, timezone, datetime } = first;
-        let ret = {
-          id: commit,
-          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
-          author,
-          date,
-          time,
-          timezone,
-          datetime,
-          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-          totalLines: lines.length,
-        };
-  
-        Object.defineProperty(ret, 'lines', {
-          value: lines,
-          // What other options do we need to set?
-          // Hint: look up configurable, writable, and enumerable
-        });
-  
-        return ret;
-      });
-  }
