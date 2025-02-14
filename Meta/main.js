add357ea1a0f23b8610f4b8b98cb3d28666ae6b1
JingJun <billy.ni222@gmail.com>
2025-02-14 17:38:59 -0300

    update csv

--- Meta/main.js
+++ Meta/main.js
@@ -11,6 +11,8 @@ async function loadData() {
       date: new Date(row.date + 'T00:00' + row.timezone),
       datetime: new Date(row.datetime),
     }));
+    processCommits();
+    console.log(commits)
   }   //This function convert all string data into correspond type of data
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -22,26 +24,26 @@ function processCommits() {
     commits = d3
       .groups(data, (d) => d.commit)
       .map(([commit, lines]) => {
-        // Each 'lines' array contains all lines modified in this commit
-        // All lines in a commit have the same author, date, etc.
-        // So we can get this information from the first line
         let first = lines[0];
         let { author, date, time, timezone, datetime } = first;
-        return {
-            id: commit,
-            url: 'https://github.com/YOUR_REPO/commit/' + commit,
-            author,
-            date,
-            time,
-            timezone,
-            datetime,
-            // Calculate hour as a decimal for time analysis
-            // e.g., 2:30 PM = 14.5
-            hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-            // How many lines were modified?
-            totalLines: lines.length,
-          };
+        let ret = {
+          id: commit,
+          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+  
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          // What other options do we need to set?
+          // Hint: look up configurable, writable, and enumerable
         });
-    }
-let a = processCommits();
-console.log(a);
+  
+        return ret;
+      });
+  }
